bcb515afd81211bab206db3b05e49c078533fde4
KMNRao <kmohanarao09@gmail.com>
2023-12-19 17:47:01 +0530

    roboshop shell scripts

--- shipping.sh
+++ shipping.sh
@@ -25,3 +25,44 @@ else
 echo "You are root user"
 fi 
 
+dnf install maven -y &>> $LOGFILE
+VALIDATE $? "Install Maven"
+
+id roboshop
+if [ $? -ne 0 ]
+then 
+ useradd roboshop
+ VALIDATE $? "roboshop user creation"
+ else 
+ echo -e "roboshop user already exist $Y SKIPPING $N"
+ fi 
+
+ curl  -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip &>> $LOGFILE
+
+ cd /app
+
+ unzip -o /tmp/shipping.zip
+ VALIDATE $? "Unziping shipping"
+ mv target/shipping-1.0.jar shipping.jar
+VALIDATE $? "moving files to shipping"
+
+cp home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service &>> $LOGFILE
+VALIDATE $? "Copying shipping.service"
+
+systemctl daemon-reload &>> $LOGFILE
+VALIDATE $? "Demon reloading"
+
+systemctl enable shipping &>> $LOGFILE
+VALIDATE $? "Enabling shipping"
+
+systemctl start shipping &>> $LOGFILE
+VALIDATE $? "Started Shipping"
+
+dnf install mysql -y &>> $LOGFILE
+VALIDATE $? "Installing Mysqlb"
+
+mysql -h mysql.mohana.cloud -uroot -pRoboShop@1 < /app/schema/shipping.sql &>> $LOGFILE
+
+systemctl restart shipping &>> $LOGFILE
+VALIDATE $? "Restarting Shipping"
+
