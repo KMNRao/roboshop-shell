f0b12d5f1aca1b254e563d0b6d981b7865e7db14
KMNRao <kmohanarao09@gmail.com>
2023-12-26 12:17:31 +0530

    ansible

---
+++ roboshop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+AMI=
+SG_ID=
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "web" "shipping" "payment" "dispatch")
+
+for i in "${INSTANCES[@b]}"
+do
+    if [ $i == "mangodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+    then
+        INSTANCE_TYPE="t3.small"
+    fi
+
+aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t2.micro --security-group-ids sg-
+done
