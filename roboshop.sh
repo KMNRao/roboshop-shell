27082123d42a68abd4819a28aed9432ab5d6322b
KMNRao <kmohanarao09@gmail.com>
2023-12-26 13:01:40 +0530

    ansible

--- roboshop.sh
+++ roboshop.sh
@@ -10,6 +10,8 @@ do
     if [ $i == "mangodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
         INSTANCE_TYPE="t3.small"
+    else
+        INSTANCE_TYPE="t2.micro"
     fi
 
 aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t2.micro --security-group-ids sg-059529b0d5245ccf4 --tag-specifications "ResourceType=instance,Tags=[{key=Name,value=$i}]"
