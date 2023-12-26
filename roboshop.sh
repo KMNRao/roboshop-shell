3710a6e7b1f45c0b56e4f9274d9b79c62d6ad3d6
KMNRao <kmohanarao09@gmail.com>
2023-12-26 13:10:48 +0530

    ansible

--- roboshop.sh
+++ roboshop.sh
@@ -2,11 +2,12 @@
 
 AMI=ami-03265a0778a880afb
 SG_ID=sg-059529b0d5245ccf4
+
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "web" "shipping" "payment" "dispatch")
 
 for i in "${INSTANCES[@]}"
 do
-    echo "instance is: $i"
+    echo "instances is: $i"
     if [ $i == "mangodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
         INSTANCE_TYPE="t3.small"
@@ -14,7 +15,6 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t2.micro --security-group-ids sg-059529b0d5245ccf4 
-#--tag-specifications "ResourceType=instance,Tags=[{key=Name,value=$i}]"
+aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-059529b0d5245ccf4 --tag-specifications "ResourceType=instances,Tags=[{key=Name,value=$i}]"
 
 done
