57954c67e0a14f1512ba1a97036c57191c185af2
KMNRao <kmohanarao09@gmail.com>
2023-12-31 06:17:26 +0530

    ansible

--- roboshop.sh
+++ roboshop.sh
@@ -15,8 +15,30 @@ do
     else
         INSTANCE_TYPE="t2.micro"
     fi
-    aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-059529b0d5245ccf4
+    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-059529b0d5245ccf4 b3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    echo "$i: $IP_ADDRESS"
+
+    #create R53 record, make sure you delete existing record
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Creating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$i'.'$DOMAIN_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP_ADDRESS'"
+            }]
+        }
+        }]
+    }
+        '
 done
+#done
     #IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-059529b0d5245ccf4 
     #done
     #--tag-specifications "ResourceType=instance,Tags=[{key=Name,value=$i}]" --query "Instances[0].privateIpAddress" --output text)
