7889388c551a16d8fa5c7265fdc48ed03daf3443
KMNRao <kmohanarao09@gmail.com>
2023-12-31 06:05:57 +0530

    ansible

--- roboshop.sh
+++ roboshop.sh
@@ -16,26 +16,26 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-059529b0d5245ccf4 --tag-specifications "ResourceType=instance,Tags=[{key=Name,value=$i}]" --query "Instances[0].privateIpAddress" --output text)
-    echo "$i: $IP_ADDRESS"
+    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-059529b0d5245ccf4 #--tag-specifications "ResourceType=instance,Tags=[{key=Name,value=$i}]" --query "Instances[0].privateIpAddress" --output text)
+#     echo "$i: $IP_ADDRESS"
     
-    aws router53 change-resources-record-sets \
-  --hosted-zone-id $ZONE_ID \
-  --change-batch '
-  {
-       "Comment": "creating a record set for cognito endpoint"
-        ,"Changes": [{
-        "Action"                : "UPSERT"
-        ,"ResourceRecordSet"    : {
-            "Name"              : "'$i'.'$DOMAIN_NAME'"
-            ,"Type"             : "A"
-            ,"TTL"              : 1
-            ,"ResourceRecord"   : [{
-                "Value"         : "'$IP_ADDRESS'"
-            }]
-        }
-        }]
-    }
-        '
+#     aws router53 change-resources-record-sets \
+#   --hosted-zone-id $ZONE_ID \
+#   --change-batch '
+#   {
+#        "Comment": "creating a record set for cognito endpoint"
+#         ,"Changes": [{
+#         "Action"                : "UPSERT"
+#         ,"ResourceRecordSet"    : {
+#             "Name"              : "'$i'.'$DOMAIN_NAME'"
+#             ,"Type"             : "A"
+#             ,"TTL"              : 1
+#             ,"ResourceRecord"   : [{
+#                 "Value"         : "'$IP_ADDRESS'"
+#             }]
+#         }
+#         }]
+#     }
+#         '
 
-done
+# done
